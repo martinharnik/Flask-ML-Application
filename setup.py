5fcf6d8852b5b21161db81c095aad80a662f2788
Martin <martin.harnik91@gmail.com>
2024-12-27 13:21:53 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    Reads requirements from a file and returns them as a list
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n", "") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+        
+
+setup(
+    name='ML-Project-01',
+    version='0.1',
+    author="Martin Harnik",
+    author_email="martin.harnik91@gmail.com",
+    packages=find_packages(),
+    install_requires=find_packages(),
+    requires=get_requirements("requirements.txt")
+)
